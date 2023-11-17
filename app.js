0786ae605b425b4977ce3f42751e1bc8c022978c
Niraj <nirajp.tt.19@nitj.ac.in>
2023-11-17 22:19:53 +0530

    connected to mongodb

--- app.js
+++ app.js
@@ -7,12 +7,36 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res, next) => {
   res.send("Working");
 });
 
+// 404 Handler
+app.use((req, res, next) => {
+  next(createHttpError.NotFound());
+});
+
+// Error Handler
+app.use((error, req, res, next) => {
+  error.status = error.status || 500;
+  res.status(error.status);
+  res.render("error_40x", { error });
+});
+
 const PORT = process.env.PORT || 3000;
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useFindAndModify: false,
+    // useCreateIndex: true,
+  })
+  .then(console.log("Connected to MongoDb"))
+  .catch((err) => {
+    console.log("connecting problem Error: ", err);
+  });
 
 app.listen(PORT, () => {
   console.log("Server is up and running on post : ", PORT);
