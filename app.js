a095e4f0cbc90a11111a74cd13f2a2c0ea8c5433
Niraj <nirajp.tt.19@nitj.ac.in>
2023-11-17 23:04:09 +0530

    created engines, public static and routes

--- app.js
+++ app.js
@@ -4,26 +4,15 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+const indexRouter = require("./routers/index.route");
+const authRouter = require("./routers/auth.route");
+const userRouter = require("./routers/user.route");
 
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
-
-app.get("/", (req, res, next) => {
-  res.send("Working");
-});
-
-// 404 Handler
-app.use((req, res, next) => {
-  next(createHttpError.NotFound());
-});
-
-// Error Handler
-app.use((error, req, res, next) => {
-  error.status = error.status || 500;
-  res.status(error.status);
-  res.render("error_40x", { error });
-});
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
 mongoose
@@ -37,6 +26,23 @@ mongoose
   .catch((err) => {
     console.log("connecting problem Error: ", err);
   });
+
+app.use("/", indexRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+// app.use("/api/index", indexRouter);
+
+// 404 Handler
+app.use((req, res, next) => {
+  next(createHttpError.NotFound());
+});
+
+// Error Handler
+app.use((error, req, res, next) => {
+  error.status = error.status || 500;
+  res.status(error.status);
+  res.render("error_40x", { error });
+});
 
 app.listen(PORT, () => {
   console.log("Server is up and running on post : ", PORT);
